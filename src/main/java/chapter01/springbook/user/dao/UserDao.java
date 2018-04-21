e939c8c75e29db7cb78a8e59323d16cc056e91ae
sw <naramp4@gmail.com>
2018-04-21 14:50:52 +0900

    77page 인터페이스의 도입

--- src/main/java/chapter01/springbook/user/dao/UserDao.java
+++ src/main/java/chapter01/springbook/user/dao/UserDao.java
@@ -6,14 +6,14 @@ import java.sql.*;
 
 public class UserDao {
 
-    private SimpleConnectionMaker simpleConnectionMaker;
+    private ConnectionMaker connectionMaker;
 
     public UserDao(){
-        simpleConnectionMaker = new SimpleConnectionMaker();
+        connectionMaker = new DConnectionMaker();
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) value(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -26,7 +26,7 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException, ClassNotFoundException {
-        Connection c = simpleConnectionMaker.makeNewConnection();
+        Connection c = connectionMaker.makeConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id =?");
         ps.setString(1, id);
@@ -46,8 +46,9 @@ public class UserDao {
     }
 
 
-    public class DUserDao extends UserDao{
-        public Connection getConnection() throws ClassNotFoundException, SQLException{
+    public class DUserDao implements ConnectionMaker{
+
+        public Connection makeConnection() throws ClassNotFoundException, SQLException {
             Class.forName("com.mysql.jdbc.Driver");
             Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "root","");
             return c;
