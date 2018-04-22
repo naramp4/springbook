11ecbe68981ab99478a3094f707369a3fdb37d40
sw <naramp4@gmail.com>
2018-04-22 21:38:38 +0900

    139page 1.8.3. DataSource 인터페이스로 변환

--- src/main/java/chapter01/springbook/user/dao/UserDao.java
+++ src/main/java/chapter01/springbook/user/dao/UserDao.java
@@ -2,18 +2,19 @@ package chapter01.springbook.user.dao;
 
 import chapter01.springbook.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
 
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
-    public void setConnectionMaker(ConnectionMaker connectionMaker){
-        this.connectionMaker = connectionMaker;
+    public void setDataSource(DataSource dataSource){
+        this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) value(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -26,7 +27,7 @@ public class UserDao {
     }
 
     public User get(String id) throws SQLException, ClassNotFoundException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement("select * from users where id =?");
         ps.setString(1, id);
