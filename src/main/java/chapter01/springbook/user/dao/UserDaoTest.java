7c2c1a4a3091fea5b93ca619caf3202047d133b8
sw <naramp4@gmail.com>
2018-04-27 15:53:31 +0900

    159page 2.2.2. 테스트의 효율적인 수행과 결과 관리

--- src/main/java/chapter01/springbook/user/dao/UserDaoTest.java
+++ src/main/java/chapter01/springbook/user/dao/UserDaoTest.java
@@ -1,15 +1,21 @@
 package chapter01.springbook.user.dao;
 
 import chapter01.springbook.user.domain.User;
+import org.junit.jupiter.api.Assertions;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
+import org.testng.annotations.Test;
 
 import java.sql.SQLException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 public class UserDaoTest {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    @Test
+    public void addAndGet() throws SQLException, ClassNotFoundException {
         ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-        UserDao dao = context.getBean("userDao",UserDao.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
         user.setId("whiteship");
@@ -18,13 +24,10 @@ public class UserDaoTest {
 
         dao.add(user);
 
-        System.out.println(user.getId() + " 등록성공");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
-        System.out.println(user2.getPassword());
-        System.out.println(user2.getId() + " 조회성공");
 
+        assertEquals(user2.getName(), user.getName());
 
     }
+
 }
